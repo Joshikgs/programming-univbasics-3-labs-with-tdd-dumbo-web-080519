0f13dbb79bae4f99f046f6147ec940347d26ae87
Josh Nho <joshnho@gmail.com>
2019-06-23 02:43:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,23 +3,10 @@ first_number = 1
 
 second_number = 2
 
+sum = first_number + second_number
 
-# Code your solution in calculator.rb
 
-describe "./calculator.rb" do
 
-  it "contains a local variable called first_number that is assigned to a number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-
-    expect(first_number).to be_an(Integer).or be_a(Float)
-  end
-
-  it "contains a local variable called second_number that is assigned to a number that isn't 0" do
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-
-    expect(second_number).to be_an(Integer).or be_a(Float)
-    expect(second_number).not_to equal(0)
-  end
 
   it "contains a local variable called sum that is assigned to the result of adding first_number and second_number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
